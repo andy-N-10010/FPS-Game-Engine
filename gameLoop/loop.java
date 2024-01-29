3750eb2a2eb072c722c2897caa0ef20dded005a3
Joshua <100229054+ajfdhagiggngg@users.noreply.github.com>
2024-01-29 09:45:12 -0500

    Update loop.java

--- gameLoop/loop.java
+++ gameLoop/loop.java
@@ -5,12 +5,25 @@ import gameState.*;
  */
 public class loop {
 
+    public void getInput() {
+        //get player input 
+    }
+
+    public void timeForUpdatingPhysics(){
+        int currentTime = ;
+        int redrawingPeriod = 20;
+        int maxFrameSkip = 10;
+        boolean needToRedraw = true;
+
+        // check if it is time to update
+    }
+    
     public static void main(String[] args) {
 
         gameEngine g = new gameState.gameEngine();
         while (!quit) {
             pollForOSMessages();
-            I = getInput(&quit);
+            I = getInput();
 
             //update as often as possible
             if (timeForUpdatingPhysics()) {
