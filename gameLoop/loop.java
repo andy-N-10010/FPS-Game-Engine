ea315d91180324f12bbeeb330c8ca4d53c1cb564
Joshua <100229054+ajfdhagiggngg@users.noreply.github.com>
2024-01-29 09:11:27 -0500

    Update loop.java
    Add info from slides

--- gameLoop/loop.java
+++ gameLoop/loop.java
@@ -8,8 +8,23 @@ public class loop {
     public static void main(String[] args) {
 
         gameEngine g = new gameState.gameEngine();
+        while (!quit) {
+            pollForOSMessages();
+            I = getInput(&quit);
 
-
+            //update as often as possible
+            if (timeForUpdatingPhysics()) {
+                G.updateGameState(I);
+            }
+            // Use to update health, ammo, used abilities, etc
+            updateStatistics();  
+            // 30 or 60 fps
+            if (timeForRendering()) {
+                G.render();
+            }
+            FPScontrol();
+        }
+        delete G;
     }
 
     
